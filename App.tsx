5e85eb3b892999ec56e2fb6071587660dcbbe45b
Miana Neagos <miananeagos@gmail.com>
2025-01-24 18:17:45 +0200

    Add Meal and MealCategory models with properties and constructors

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { MealCategories, MealsData } from './data/data';
 
 export default function App() {
+  // console.log('MealsData: ', MealsData);
+  // console.log('Category Data: ', MealCategories);
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={{color: '#fff'}}>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
@@ -13,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ models/meal_category.ts
@@ -0,0 +1,17 @@
+class MealCategory {
+    id: string;
+    title: string;
+    color: string;
+
+    constructor(
+        id: string,
+        title: string,
+        color: string
+    ) {
+        this.id = id;
+        this.title = title;
+        this.color = color;
+    }
+}
+
+export default MealCategory;
