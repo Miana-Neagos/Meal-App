7a9f7386a66eb5bd8588afe1421d207efa9f2a42
Miana Neagos <miananeagos@gmail.com>
2025-01-24 21:09:52 +0200

    Add navigation setup and dependencies

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen></CategoriesScreen>
+    <StatusBar style="light" />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="MealsCategories" component={CategoriesScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   // alignItems: "center",
-  //   // justifyContent: "center",
-  // },
 });
