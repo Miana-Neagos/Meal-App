0c5c709783a3c6b64fa150db2dc60cfe82aead73
Miana Neagos <miananeagos@gmail.com>
2025-02-12 19:17:03 +0200

    Refactor FavoritesScreen to use Redux for favorite meals; remove context usage and update meal filtering logic.

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,13 +1,15 @@
 import { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { FavoritesContext } from "../store/context/favoritesContext";
+// import { FavoritesContext } from "../store/context/favoritesContext";
 import MealsList from "../components/MealsList/MealsList";
 import { MealsData } from "../data/data";
 import { colorTheme } from "../colorTheme";
+import { useAppSelector } from "../store/redux/hooks";
 
 const FavoritesScreen = () => {
-  const favsContext = useContext(FavoritesContext);
-  const favMeals = MealsData.filter((meal) => favsContext.ids.includes(meal.id));
+  const favMealIds = useAppSelector((state) => state.favoriteMeals.ids);
+  // const favsContext = useContext(FavoritesContext);
+  const favMeals = MealsData.filter((meal) => favMealIds.includes(meal.id));
 
   return (
     favMeals.length > 0 ? <MealsList mealsData={favMeals} /> : <View style={styles.container}><Text style={styles.favText}>No favorites added</Text></View>
